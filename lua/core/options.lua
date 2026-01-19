d5fd11f10b843055d95afbe0c6a5035ba35d3d69
Himanshu Gohil <himanshugohil234@gmail.com>
2026-01-19 17:55:03 +0000

    Added 80 column guide

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -41,3 +41,5 @@ vim.opt.shortmess:append 'c' -- Don't give |ins-completion-menu| messages (defau
 vim.opt.iskeyword:append '-' -- Hyphenated words recognized by searches (default: does not include '-')
 vim.opt.formatoptions:remove { 'c', 'r', 'o' } -- Don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode. (default: 'croql')
 vim.opt.runtimepath:remove '/usr/share/vim/vimfiles' -- Separate Vim plugins from Neovim in case Vim still in use (default: includes this path if Vim is installed)
+vim.opt.colorcolumn = "80" -- Add an indicator after 80 lines
+-- vim.opt.textwidth = 80; -- Auto line break after 80
