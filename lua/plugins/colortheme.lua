e64d89a0445a0ac35f476a4e7c579d29c54a0016
Himanshu Gohil <himanshugohil234@gmail.com>
2026-01-22 11:43:38 +0000

    Updated colortheme

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,50 +1,61 @@
 return {
-    -- NOTE: Active
-    {
-        "bluz71/vim-moonfly-colors",
-        name = "moonfly",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            -- Toggle background transparency
-            local bg_transparent = true
+  -- NOTE: Active
+  {
+    'bluz71/vim-moonfly-colors',
+    name = 'moonfly',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Toggle background transparency
+      local bg_transparent = true
 
-            local toggle_transparency = function()
-                bg_transparent = not bg_transparent
-                vim.g.moonflytransparent = bg_transparent
-                vim.cmd [[colorscheme moonfly]]
-            end
+      local toggle_transparency = function()
+        bg_transparent = not bg_transparent
+        vim.g.moonflytransparent = bg_transparent
+        vim.cmd [[colorscheme moonfly]]
+      end
 
-            vim.keymap.set('n', '<leader>bg', toggle_transparency, { noremap = true, silent = true })
+      vim.keymap.set('n', '<leader>bg', toggle_transparency, { noremap = true, silent = true })
 
-            vim.cmd.colorscheme 'moonfly'
-        end
-    },
-    -- Extra Color Schemes Tried
-    -- {
-    --     'folke/tokyonight.nvim',
-    --     priority = 1000, -- Make sure to load this before all the other start plugins.
-    --     config = function()
-    --         ---@diagnostic disable-next-line: missing-fields
-    --         require('tokyonight').setup {
-    --             styles = {
-    --                 comments = { italic = false }, -- Disable italics in comments
-    --             },
-    --         }
-    --
-    --         -- Load the colorscheme here.
-    --         -- Like many other themes, this one has different styles, and you could load
-    --         -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    --     end,
-    -- },
-    -- {
-    --     "navarasu/onedark.nvim",
-    --     priority = 1000, -- make sure to load this before all the other start plugins
-    --     config = function()
-    --         require('onedark').setup {
-    --             style = 'warmer'
-    --         }
-    --         require('onedark').load()
-    --     end
-    -- },
+      vim.cmd.colorscheme 'moonfly'
+    end,
+    init = function()
+      vim.g.moonflyVirtualTextColor = true
+    end,
+  },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    -- config = function()
+    --   vim.cmd.colorscheme = 'catppuccin'
+    -- end,
+  },
+  -- Extra Color Schemes Tried
+  -- {
+  --     'folke/tokyonight.nvim',
+  --     priority = 1000, -- Make sure to load this before all the other start plugins.
+  --     config = function()
+  --         ---@diagnostic disable-next-line: missing-fields
+  --         require('tokyonight').setup {
+  --             styles = {
+  --                 comments = { italic = false }, -- Disable italics in comments
+  --             },
+  --         }
+  --
+  --         -- Load the colorscheme here.
+  --         -- Like many other themes, this one has different styles, and you could load
+  --         -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --     end,
+  -- },
+  -- {
+  --     "navarasu/onedark.nvim",
+  --     priority = 1000, -- make sure to load this before all the other start plugins
+  --     config = function()
+  --         require('onedark').setup {
+  --             style = 'warmer'
+  --         }
+  --         require('onedark').load()
+  --     end
+  -- },
 }
