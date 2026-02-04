bcdc8d63f9150990ac523799ca4d7936184c5387
Himanshu Gohil <himanshugohil234@gmail.com>
2026-02-04 03:41:13 +0000

    Updated the alpha nvim

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -3,10 +3,45 @@ return {
   -- dependencies = { 'nvim-mini/mini.icons' },
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    local startify = require 'alpha.themes.startify'
+    -- local startify = require 'alpha.themes.startify'
     -- available: devicons, mini, default is mini
     -- if provider not loaded and enabled is true, it will try to use another provider
-    startify.file_icons.provider = 'devicons'
-    require('alpha').setup(startify.config)
+    -- startify.file_icons.provider = 'devicons'
+    -- require('alpha').setup(startify.config)
+
+    local alpha = require 'alpha'
+    local dashboard = require 'alpha.themes.dashboard'
+    -- local dashboard = require 'alpha.themes.dashboard'
+
+    -- Set header
+    dashboard.section.header.val = {
+      '                                                     ',
+      '  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ',
+      '  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ',
+      '  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ',
+      '  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ',
+      '  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ',
+      '  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ',
+      '                                                     ',
+    }
+
+    -- Set menu
+    dashboard.section.buttons.val = {
+      dashboard.button('e', '  > New file', ':ene <BAR> startinsert <CR>'),
+      dashboard.button('f', '  > Find file', ':cd $HOME/Workspace | Telescope find_files<CR>'),
+      dashboard.button('r', '  > Recent', ':Telescope oldfiles<CR>'),
+      dashboard.button('s', '  > Settings', ':e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>'),
+      dashboard.button('q', '  > Quit NVIM', ':qa<CR>'),
+    }
+
+    -- Set footer
+    --   NOTE: This is currently a feature in my fork of alpha-nvim (opened PR #21, will update snippet if added to main)
+    --   To see test this yourself, add the function as a dependecy in packer and uncomment the footer lines
+    --   ```init.lua
+    --   return require('packer').startup(function()
+    --       use 'wbthomason/packer.nvim'
+    --       use {
+    --
+    alpha.setup(dashboard.config)
   end,
 }
