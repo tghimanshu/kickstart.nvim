d81b9bad8d3676404b0ef92dccf0b053f1939d05
Himanshu Gohil <himanshugohil234@gmail.com>
2026-02-08 23:58:24 +0530

    disabled flash.nvim

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -180,54 +180,59 @@ return {
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
-  {
-    'folke/flash.nvim',
-    event = 'VeryLazy',
-    ---@type Flash.Config
-    opts = {},
-    keys = {
-      {
-        's',
-        mode = { 'n', 'x', 'o' },
-        function()
-          require('flash').jump()
-        end,
-        desc = 'Flash',
-      },
-      {
-        'S',
-        mode = { 'n', 'x', 'o' },
-        function()
-          require('flash').treesitter()
-        end,
-        desc = 'Flash Treesitter',
-      },
-      {
-        'r',
-        mode = 'o',
-        function()
-          require('flash').remote()
-        end,
-        desc = 'Remote Flash',
-      },
-      {
-        'R',
-        mode = { 'o', 'x' },
-        function()
-          require('flash').treesitter_search()
-        end,
-        desc = 'Treesitter Search',
-      },
-      {
-        '<c-s>',
-        mode = { 'c' },
-        function()
-          require('flash').toggle()
-        end,
-        desc = 'Toggle Flash Search',
-      },
-    },
-  },
+  -- {
+  --   'folke/flash.nvim',
+  --   event = 'VeryLazy',
+  --   ---@type Flash.Config
+  --   opts = {},
+  --   modes = {
+  --     search = {
+  --       enabled = false,
+  --     },
+  --   },
+  --   keys = {
+  --     {
+  --       's',
+  --       mode = { 'n', 'x', 'o' },
+  --       function()
+  --         require('flash').jump()
+  --       end,
+  --       desc = 'Flash',
+  --     },
+  --     {
+  --       'S',
+  --       mode = { 'n', 'x', 'o' },
+  --       function()
+  --         require('flash').treesitter()
+  --       end,
+  --       desc = 'Flash Treesitter',
+  --     },
+  --     {
+  --       'r',
+  --       mode = 'o',
+  --       function()
+  --         require('flash').remote()
+  --       end,
+  --       desc = 'Remote Flash',
+  --     },
+  --     {
+  --       'R',
+  --       mode = { 'o', 'x' },
+  --       function()
+  --         require('flash').treesitter_search()
+  --       end,
+  --       desc = 'Treesitter Search',
+  --     },
+  --     {
+  --       '<c-s>',
+  --       mode = { 'c' },
+  --       function()
+  --         require('flash').toggle()
+  --       end,
+  --       desc = 'Toggle Flash Search',
+  --     },
+  --   },
+  -- },
   { -- markdown preview
     'OXY2DEV/markview.nvim',
     lazy = false,
